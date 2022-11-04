d00871ac44e0b093223729b77ad6add7d7a44923
Jorge Martinez Garrido <jorge.martinezgarrido@ansys.com>
2022-11-04 10:03:24 +0100

    DOC: add PDF rules

--- doc/make.bat
+++ doc/make.bat
@@ -1,4 +1,5 @@
-@ECHO OFF
+@echo OFF
+setlocal
 
 pushd %~dp0
 
@@ -10,28 +11,40 @@ if "%SPHINXBUILD%" == "" (
 set SOURCEDIR=source
 set BUILDDIR=_build
 
+if "%1" == "" goto help
+if "%1" == "clean" goto clean
+if "%1" == "pdf" goto pdf
+
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
 	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
 	echo.installed, then set the SPHINXBUILD environment variable to point
 	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to the PATH environment variable.
-
+	echo.may add the Sphinx directory to PATH.
 	echo.
-	echo.If you don't have Sphinx installed, you can obtain it from
-
-	echo.https://www.sphinx-doc.org/
+	echo.If you don't have Sphinx installed, grab it from
+	echo.http://sphinx-doc.org/
 	exit /b 1
 )
 
-if "%1" == "" goto help
-
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
+:clean
+rmdir /s /q %BUILDDIR% > /NUL 2>&1 
+for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
 goto end
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
+:pdf
+	%SPHINXBUILD% -M latex %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+	cd "%BUILDDIR%\latex"
+	for %%f in (*.tex) do (
+	pdflatex "%%f" --interaction=nonstopmode)
+
 :end
 popd
+
