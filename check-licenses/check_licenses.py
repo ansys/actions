496ad9475105a7a746dabec78531d44554c892a5
guillem.barroso <guillem.barroso@ansys.com>
2022-11-24 19:23:52 +0100

    Store licenses in env var and print from .py file

--- check-licenses/check_licenses.py
+++ check-licenses/check_licenses.py
@@ -1,12 +1,18 @@
+import os
+
+
 class LicenseError(Exception):
     pass
 
+
 def check_licenses():
+    print("Printing licenses:")
+    print(os.environ.get("LICENSES"))
     message = """
     At least one license is not accepted. The list of unaccepted \
     licenses are: ['test license', 'more licenses']"""
-    raise(LicenseError(message))
+    raise (LicenseError(message))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check_licenses()
