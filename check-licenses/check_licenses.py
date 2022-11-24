5b6a1c7ee7e7f3f567e28bbee2ac25e51048cf33
guillem.barroso <guillem.barroso@ansys.com>
2022-11-24 17:27:48 +0100

    Add custom LicenseError with dummy message

--- check-licenses/check_licenses.py
+++ check-licenses/check_licenses.py
@@ -1,6 +1,10 @@
+class LicenseError(Exception):
+    pass
 
 def check_licenses():
-    raise(ValueError)
+    message = "At least one license is not accepted. The list of unaccepted \
+    licenses are: ['test license', 'more licenses']"
+    raise(LicenseError(message))
 
 
 if __name__ == '__main__':
