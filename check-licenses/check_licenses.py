c3d1f4445761090c42615fb1ec47e6896f9105fe
guillem.barroso <guillem.barroso@ansys.com>
2022-11-24 21:08:32 +0100

    list all available env vars and improve message

--- check-licenses/check_licenses.py
+++ check-licenses/check_licenses.py
@@ -1,17 +1,21 @@
 import os
 
+ERROR_MESSAGE = """
+At least one license is not accepted. The list of invalid \
+licenses are: {invalid_licenses}"""
+
 
 class LicenseError(Exception):
     pass
 
 
 def check_licenses():
+    print("Printing all env variables:")
+    print(os.environ)
     print("Printing licenses:")
     print(os.environ.get("LICENSES"))
-    message = """
-    At least one license is not accepted. The list of unaccepted \
-    licenses are: ['test license', 'more licenses']"""
-    raise (LicenseError(message))
+    dummy_list = ["random lib", "more libs"]
+    raise (LicenseError(ERROR_MESSAGE.format(invalid_licenses=dummy_list)))
 
 
 if __name__ == "__main__":
