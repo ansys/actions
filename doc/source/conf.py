c1c1856fea6d2aa43aa14a778129c481cc7ff1d8
Jorge Martínez Garrido <jorge.martinezgarrido@ansys.com>
2022-12-02 21:09:04 +0100

    FEAT: filter out actions per prefix and suffix

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -7,6 +7,14 @@ import yaml
 from ansys_sphinx_theme import ansys_favicon, ansys_logo_black, get_version_match
 from tabulate import tabulate as Table
 
+# Constants used for generating documentation
+DOC_SOURCE_DIR = pathlib.Path(__file__).parent.parent
+DOC_DIR = DOC_SOURCE_DIR.parent
+BASE_DIR = DOC_SOURCE_DIR.parent
+ACTIONS_PREFIXES = ("build-", "check-", "doc-", "release-", "tests-")
+ACTIONS_SUFFIXES = "-style"
+ACTIONS_INPUTS_FIELDS = ("description", "required", "type", "default")
+
 # Project information
 project = "PyAnsys Actions"
 copyright = "(c) 2022 ANSYS, Inc. All rights reserved"
@@ -61,13 +69,48 @@ master_doc = "index"
 autosectionlabel_maxdepth = 4
 
 
-def action_file_inputs_to_rst_table(action_name):
-    """Read and convert input variables of a GitHub YAML action file into a RST table.
+# Auxiliary routines for automatic documentation generation
+
+
+def is_valid_action_dir(path):
+    """Verify if a directory is a valid action directory.
+
+    The directory is valid if it has a valid action pattern name and contains
+    an ``action.yml`` file.
 
     Parameters
     ----------
-    action_file : str
-        Name of the action file.
+    path : ~pathlib.Path
+        The ``Path`` instance to verify if contains an ``action.yml`` and has a valid pattern.
+
+    Returns
+    -------
+    bool
+        Returns ``True`` if the directory is a valid one, ``False`` otherwise.
+
+    """
+    # Verify that the path is a directory and not a file
+    if not path.is_dir():
+        return False
+
+    # Verify that the path contains an action.yml file
+    if not (path / "action.yml").exists():
+        return False
+
+    # Verify if is a public and registered action
+    if path.name.startswith(ACTIONS_PREFIXES) or path.name.endswith(ACTIONS_SUFFIXES):
+        return True
+
+    return False
+
+
+def generate_inputs_table_from_action_file(action_file):
+    """Generate the RST table containing all the input information for the action.
+
+    Parameters
+    ----------
+    action_file : ~pathlib.Path
+        A ``Path`` object representing the action file.
 
     Returns
     -------
@@ -75,11 +118,11 @@ def action_file_inputs_to_rst_table(action_name):
         String representing the RST table.
 
     """
-    field_names = ["input", "description", "required", "type", "default"]
+    field_names = ("input",) + ACTIONS_INPUTS_FIELDS
     headers = [field.capitalize() for field in field_names]
     table_content = []
 
-    with open(f"../../{action_name}/action.yml", "r") as yaml_file:
+    with open(action_file, "r") as yaml_file:
         file_content = yaml.safe_load(yaml_file)
         inputs = file_content["inputs"]
         for input_name, values in inputs.items():
@@ -92,27 +135,17 @@ def action_file_inputs_to_rst_table(action_name):
         return str(Table(table_content, headers=headers, tablefmt="grid"))
 
 
-# Generate documentation for the action inputs in a dynamic way
-ACTIONS_NAMES = [
-    "code-style",
-    "build-library",
-    "build-wheelhouse",
-    "build-ci-wheels",
-    "doc-style",
-    "doc-build",
-    "doc-deploy-dev",
-    "doc-deploy-stable",
-    "doc-deploy-to-repo",
-    "release-pypi-private",
-    "release-pypi-test",
-    "release-pypi-public",
-    "release-github",
-    "tests-pytest",
-    "check-licenses",
-]
-jinja_contexts = {
-    action_name: {"inputs_table": action_file_inputs_to_rst_table(action_name)}
-    for action_name in ACTIONS_NAMES
+# Collect all public actions directories and files
+public_actions = {
+    action_dir: action_dir / "action.yml"
+    for action_dir in BASE_DIR.iterdir()
+    if is_valid_action_dir(action_dir)
 }
 
-# Generate examples based on real YML files and guarantee input parameters
+# Generate documentation for the action inputs in a dynamic way
+jinja_contexts = {
+    action_dir.name: {
+        "inputs_table": generate_inputs_table_from_action_file(action_file)
+    }
+    for action_dir, action_file in public_actions.items()
+}
