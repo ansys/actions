946169187776ae2ec9b73368974c0c8e591e3d8a
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-04-05 09:41:30 +0200

    feat: Add the wait time

--- doc-deploy-dev/versions.js
+++ doc-deploy-dev/versions.js
@@ -1,28 +1,29 @@
-async function updateContent() {
-  try {
-    const [htmlResponse, jsonResponse] = await Promise.all([
+
+(async function() {
+    try {
+      const [htmlResponse, jsonResponse] = await Promise.all([
       fetch("index.html"),
       fetch("versions.json")
-    ]);
+      ]);
 
-    if (!htmlResponse.ok) {
-      throw new Error(`HTTP error: ${htmlResponse.status}`);
+      if (!htmlResponse.ok) {
+        throw new Error(`HTTP error: ${htmlResponse.status}`);
+      }
+
+      const html = await htmlResponse.text();
+      document.documentElement.innerHTML = html;
+
+      const versionsData = await jsonResponse.json();
+      const table = createVersionsTable(versionsData);
+      replaceArticleWithNewRelease(table);
+
+      removeSidebarFromPage();
+    } catch (error) {
+      console.error(error);
     }
+  })();
 
-    const html = await htmlResponse.text();
-    document.documentElement.innerHTML = html;
-
-    const versionsData = await jsonResponse.json();
-    const table = createVersionsTable(versionsData);
-    replaceArticleWithNewRelease(table);
-
-    removeSidebarFromPage();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function createVersionsTable(data) {
+  function createVersionsTable(data) {
   let table = "<h2>Older versions</h2>";
   table += "<table class='table'>";
   table += "<thead><tr><th> # </th><th> Version </th><th> URL </th></tr></thead>";
@@ -37,18 +38,18 @@ function createVersionsTable(data) {
   }
   table += "</tbody></table>";
   return table;
-}
+  }  
+  function replaceArticleWithNewRelease(table) {
+    const article = document.querySelector("article");
+    const release = document.createElement("release");
+    release.innerHTML = table;
+    article.parentNode.replaceChild(release, article);
+  }
 
-function replaceArticleWithNewRelease(table) {
-  const article = document.querySelector("article");
-  const release = document.createElement("release");
-  release.innerHTML = table;
-  article.parentNode.replaceChild(release, article);
-}
-
-function removeSidebarFromPage() {
-  const sidebar = document.querySelector(
-    "body > div.bd-container.container-xl > div > div.bd-sidebar-secondary.d-none.d-xl-block.col-xl-2.bd-toc"
-  );
-  sidebar.parentNode.removeChild(sidebar);
-}
+  function removeSidebarFromPage() {
+    const sidebar = document.querySelector(
+      "body > div.bd-container.container-xl > div > div.bd-sidebar-secondary.d-none.d-xl-block.col-xl-2.bd-toc"
+    );
+    sidebar.parentNode.removeChild(sidebar);
+  }
+  
