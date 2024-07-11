b950aaa8701af3e01057f9f259d82d4bc5aaebf0
klmcadams <58492561+klmcadams@users.noreply.github.com>
2024-07-11 12:08:14 -0400

    add docstrings & separate function into smaller functions

--- doc-changelog/parse_pr_title.py
+++ doc-changelog/parse_pr_title.py
@@ -3,49 +3,90 @@ import os
 import toml
 
 
-def save_env_variable(env_var_name, env_var_value):
+def save_env_variable(env_var_name: str, env_var_value: str):
+    """Save environment variable to the GITHUB_ENV file.
+
+    Parameters
+    ----------
+    env_var_name: str
+        The name of the environment variable.
+    env_var_value: str
+        The value of the environment variable.
+    """
     # Get the GITHUB_ENV variable
     github_env = os.getenv("GITHUB_ENV")
 
     # Save environment variable with its value
-    with open(github_env, "a") as f:
-        f.write(f"{env_var_name}={env_var_value}")
+    with open(github_env, "a") as file:
+        file.write(f"{env_var_name}={env_var_value}")
 
 
-def get_first_letter_case(pr_title):
-    pr_title = f"""{pr_title}"""
+def get_first_letter_case(pr_title: str):
+    """Get the first letter of the pull request title and determine if it is uppercase or not.
+
+    Parameters
+    ----------
+    pr_title: str
+        The pull request title.
+    """
     index = 0
+    # Get the pull request title
+    pr_title = f"""{pr_title}"""
+    # Get the first letter of the pull request title
     first_letter = pr_title[index]
 
-    print(pr_title)
-
+    # If the pull request title starts with a blank space, move to the next index
+    # until it finds a letter
     while first_letter == " ":
         index += 1
         try:
+            # Set the first letter
             first_letter = pr_title[index]
         except IndexError:
+            # If the pull request title never finds a letter, the pull request title
+            # is blank
             print("Pull request title is blank")
             exit(1)
 
+    # If the first letter is lowercase, save the FIRST_LETTER environment variable
+    # as lowercase. Otherwise, save it as uppercase
     if first_letter.islower():
         save_env_variable("FIRST_LETTER", "lowercase")
     else:
         save_env_variable("FIRST_LETTER", "uppercase")
 
 
-def get_conventional_commit_type(pr_title):
+def get_conventional_commit_type(pr_title: str):
+    """Get the conventional commit type from the pull request title.
+
+    Parameters
+    ----------
+    pr_title: str
+        The pull request title.
+    """
+    # Get the pull request title
     pr_title = f"""{pr_title}"""
+    # Get the index where the first colon is found in the pull request title
     colon_index = pr_title.index(":")
+    # Get the conventional commit type from the pull request title (everything before the colon)
     cc_type = '"' + pr_title[:colon_index] + '"'
+    # Save the conventional commit type as an environment variable, CC_TYPE
     save_env_variable("CC_TYPE", cc_type)
 
 
-def changelog_category_cc(cc_type):
-    # Get conventional commit type from env variable
+def changelog_category_cc(cc_type: str):
+    """Get the changelog category based on the conventional commit type.
+
+    Parameters
+    ----------
+    cc_type: str
+        The conventional commit type from the pull request title.
+    """
+    # Get conventional commit type from the environment variable
     cc_type = cc_type.lower()
 
-    print(cc_type)
-
+    # Dictionary whose keys are the conventional commit type and values are
+    # the changelog section
     cc_type_changelog_dict = {
         "feat": "added",
         "fix": "fixed",
@@ -60,12 +101,21 @@ def changelog_category_cc(cc_type):
         "ci": "maintenance",
     }
 
+    # Get the changelog section based on the conventional commit type
     changelog_section = cc_type_changelog_dict[cc_type]
 
+    # Save the changelog section to the CHANGELOG_SECTION environment variable
     save_env_variable("CHANGELOG_SECTION", changelog_section)
 
 
-def changelog_cateogry_labels(labels):
+def changelog_cateogry_labels(labels: str):
+    """Get the changelog category based on the labels in the pull request.
+
+    Parameters
+    ----------
+    labels: str
+        String containing the labels in the pull request.
+    """
     # Create a list of labels found in the pull request
     # For example, "enhancement maintenance".split() -> ["enhancement", "maintenance"]
     existing_labels = labels.split()
@@ -82,25 +132,52 @@ def changelog_cateogry_labels(labels):
         "maintenance": "maintenance",
     }
 
-    def get_changelog_section(pr_labels, existing_labels):
-        """Find the changelog section corresponding to the label in the PR."""
-        label_type = ""
-
-        for key, value in pr_labels.items():
-            if key in existing_labels:
-                label_type = value
-                return label_type
-
-        # If no labels are in the PR, it goes into the miscellaneous category
-        label_type = "miscellaneous"
-        return label_type
-
+    # Save the changelog section to the CHANGELOG_SECTION environment variable
     save_env_variable(
         "CHANGELOG_SECTION", get_changelog_section(pr_labels, existing_labels)
     )
 
 
-def clean_pr_title(pr_title, use_labels):
+def get_changelog_section(pr_labels: dict, existing_labels: list) -> str:
+    """Find the changelog section corresponding to the label in the pull request.
+
+    Parameters
+    ----------
+    pr_labels: dict
+        Dictionary containing pull request labels and their corresponding changelog sections.
+    existing_labels: list
+        List of the labels that are in the pull request.
+
+    Returns
+    -------
+    str
+        The changelog section.
+    """
+    changelog_section = ""
+
+    # For each label key and changelog section value
+    for key, value in pr_labels.items():
+        # If the label is in the existing_labels list
+        if key in existing_labels:
+            # Save the changelog section based on the label
+            changelog_section = value
+            return changelog_section
+
+    # If no labels are in the PR, it goes into the miscellaneous category
+    changelog_section = "miscellaneous"
+    return changelog_section
+
+
+def clean_pr_title(pr_title: str, use_labels: str):
+    """Clean the pull request title.
+
+    Parameters
+    ----------
+    pr_title: str
+        The pull request title.
+    use_labels: str
+        Whether or not to use labels to get the changelog section.
+    """
     # Retrieve title
     clean_title = pr_title
 
@@ -109,23 +186,35 @@ def clean_pr_title(pr_title, use_labels):
         colon_index = clean_title.index(":")
         clean_title = clean_title[colon_index + 1 :]
 
-    print(clean_title)
-
     # Remove extra whitespace
     clean_title = clean_title.strip()
 
     # Add backslash in front of backtick and double quote
     clean_title = clean_title.replace("`", "\\`").replace('"', '\\"')
 
+    # Save the clean pull request title as the CLEAN_TITLE environment variable
     save_env_variable("CLEAN_TITLE", clean_title)
 
 
-def add_towncrier_config(org_name, repo_name, web_release_notes):
+def add_towncrier_config(org_name: str, repo_name: str, web_release_notes: bool):
+    """Append the missing towncrier information to the pyproject.toml file.
+
+    Parameters
+    ----------
+    org_name: str
+        The name of the organization.
+    repo_name: str
+        The name of the repository.
+    web_release_notes: bool
+        Whether or not the release notes are in the online documentation or the repository.
+    """
     # Load pyproject.toml file
     with open("pyproject.toml", "a+") as file:
         config = toml.load("pyproject.toml")
         tool = config.get("tool", "DNE")
         towncrier = tool.get("towncrier", "DNE")
+
+        # List containing changelog sections under each release
         changelog_sections = [
             "added",
             "dependencies",
@@ -138,67 +227,126 @@ def add_towncrier_config(org_name, repo_name, web_release_notes):
 
         # Get the package name from [tool.flit.module]
         flit = tool.get("flit", "DNE")
-        module = name = package = ""
+        module = name = ""
         if flit != "DNE":
             module = flit.get("module", "DNE")
             if module != ("DNE" or ""):
                 name = module.get("name", "DNE")
 
+        # Dictionary containing [tool.towncrier] keys and values
+        towncrier_config_sections = {
+            "directory": '"doc/changelog.d"',
+            "template": '"doc/changelog.d/changelog_template.jinja"',
+            "filename": {"web": '"doc/source/changelog.rst"', "repo": '"CHANGELOG.md"'},
+            "start_string": {
+                "web": '".. towncrier release notes start\\n"',
+                "repo": '"<!-- towncrier release notes start -->\\n"',
+            },
+            "title_format": {
+                "web": f'"`{{version}} <https://github.com/{org_name}/{repo_name}/releases/tag/v{{version}}>`_ - {{project_date}}"',
+                "repo": f'"## [{{version}}](https://github.com/{org_name}/{repo_name}/releases/tag/v{{version}}) - {{project_date}}"',
+            },
+            "issue_format": {
+                "web": f'"`#{{issue}} <https://github.com/{org_name}/{repo_name}/pull/{{issue}}>`_"',
+                "repo": f'"[#{{issue}}](https://github.com/{org_name}/{repo_name}/pull/{{issue}})"',
+            },
+        }
+
         # If [tool.flit.module] name exists, create the package string
         if name != ("DNE" and ""):
-            package = f'package = "{name}"'
+            towncrier_config_sections["package"] = f'"{name}"'
 
-        # If the [tool.towncrier] section does not exist
         if towncrier == "DNE":
-            # Append the tool.towncrier section
-            file.write("\n[tool.towncrier]\n")
-
-            # Write the package line if [tool.flit.module] name exists
-            if package:
-                file.write(f"{package}\n")
-
-            # Write general towncrier configuration
-            file.write(
-                """directory = "doc/changelog.d"
-underlines = ["", "", ""]
-template = "doc/changelog.d/changelog_template.jinja"\n"""
+            # Write the [tool.towncrier] and [[tool.towncrier.type]] sections
+            write_towncrier_config_section(
+                file, towncrier_config_sections, web_release_notes
             )
-
-            # Write configuration for changelog.rst release notes
-            if web_release_notes:
-                file.write(
-                    f"""filename = "doc/source/changelog.rst"
-start_string = ".. towncrier release notes start\\n"
-title_format = "`{{version}} <https://github.com/{org_name}/{repo_name}/releases/tag/v{{version}}>`_ - {{project_date}}"
-issue_format = "`#{{issue}} <https://github.com/{org_name}/{repo_name}/pull/{{issue}}>`_"\n"""
-                )
-
-            # Write configuration for CHANGELOG.md release notes
-            else:
-                file.write(
-                    f"""filename = "CHANGELOG.md"
-start_string = "<!-- towncrier release notes start -->\\n"
-title_format = "## [{{version}}](https://github.com/{org_name}/{repo_name}/releases/tag/v{{version}}) - {{project_date}}"
-issue_format = "[#{{issue}}](https://github.com/{org_name}/{repo_name}/pull/{{issue}})"\n"""
-                )
-        # If [tool.towncrier] exists
+            write_missing_types(changelog_sections, file)
         else:
             # Get the [[tool.towncrier.type]] sections
             types = towncrier.get("type", "DNE")
 
-            # If the [[tool.towncrier.type]] sections exist
-            if types != "DNE":
-                for group in types:
-                    # Remove changelog section if it exists under [[tool.towncrier.type]] so that
-                    # only missing sections are appended to the pyproject.toml file
-                    changelog_sections.remove(group["directory"])
+            # Add missing [[tool.towncrier.type]] sections and entire [tool.towncrier] section
+            if len(towncrier) == 1 and (types != "DNE"):
+                remove_existing_types(types, changelog_sections)
+                write_missing_types(changelog_sections, file)
+                write_towncrier_config_section(
+                    file, towncrier_config_sections, web_release_notes
+                )
+            else:
+                # Add missing [[tool.towncrier.type]]
+                remove_existing_types(types, changelog_sections)
+                write_missing_types(changelog_sections, file)
+                # Add missing lines of [tool.towncrier] section?
 
-        # Write each missing section to the pyproject.toml file
-        for section in changelog_sections:
-            file.write(
-                f"""
+
+def write_towncrier_config_section(
+    file, towncrier_config_sections: dict, web_release_notes: bool
+):
+    """Write the information in the [tool.towncrier] section.
+
+    Parameters
+    ----------
+    file: _io.TextIOWrapper
+        File to write to.
+    towncrier_config_sections: dict
+        Dictionary containing the [tool.towncrier] keys and values.
+    web_release_notes: bool
+        Whether or not the release notes are in the online documentation or the repository.
+    """
+    # Append the tool.towncrier section
+    file.write("\n[tool.towncrier]\n")
+
+    # For each key and value in the towncrier_config_sections dictionary
+    for key, value in towncrier_config_sections.items():
+        # If the key has values that depend on the web_release_notes boolean
+        if (
+            key == "filename"
+            or key == "start_string"
+            or key == "title_format"
+            or key == "issue_format"
+        ):
+            # Select the value based on the web_release_notes boolean
+            if web_release_notes:
+                file.write(f'{key} = {value["web"]}\n')
+            else:
+                file.write(f'{key} = {value["repo"]}\n')
+        else:
+            # Write the key and value from the towncrier_config_sections dictionary
+            file.write(f"{key} = {value}\n")
+
+
+def remove_existing_types(types, changelog_sections: list):
+    """Remove the existing [[tool.towncrier.types]] from the changelog_sections list.
+
+    Parameters
+    ----------
+    types: list
+        List of dictionaries containing information under the [[tool.towncrier.types]] sections.
+    changelog_sections: list
+        List containing changelog sections under each release.
+    """
+    for group in types:
+        # Remove changelog section if it exists under [[tool.towncrier.type]] so that
+        # only missing sections are appended to the pyproject.toml file
+        changelog_sections.remove(group["directory"])
+
+
+def write_missing_types(changelog_sections: list, file):
+    """Write the missing types in [[tool.towncrier.types]]
+
+    Parameters
+    ----------
+    changelog_sections: list
+        List containing changelog sections under each release.
+    file
+    """
+    # Write each missing section to the pyproject.toml file
+    for section in changelog_sections:
+        file.write(
+            f"""
 [[tool.towncrier.type]]
 directory = "{section}"
 name = "{section.title()}"
 showcontent = true\n"""
-            )
+        )
