bd50224915e503d0632623117519073e6032f419
klmcadams <58492561+klmcadams@users.noreply.github.com>
2024-10-30 16:00:23 -0400

    add more comments

--- python-utils/release_github_utils.py
+++ python-utils/release_github_utils.py
@@ -21,18 +21,23 @@ def get_pattern(content, section_title_regex):
     vale_regex = r"(.. vale)"
     eof_regex = r"\Z"
 
-    # Get number of sections
+    # Find all strings with the following format:
+    # `#.#.# <https://github.com/ansys/.../releases/tag/v#.#.#>`_ OR
+    # ## [#.#.#](https://github.com/ansys/.../releases/tag/v#.#.#) - 2024-10-30
     sections = re.findall(section_title_regex, content)
 
     if sections:
         # If there is only one section
         if len(sections) == 1:
             if "vale on" in content:
+                # Get all text from the section title to the ".. vale on" line
                 pattern = rf"({section_title_regex})+([\W\w]*?){vale_regex}"
             else:
+                # Get all text from the section title to the end of the file
                 pattern = rf"({section_title_regex})+([\W\w]*?){eof_regex}"
         # If there is more than one section
         elif len(sections) > 1:
+            # Get the section title and its content up to the next section title
             pattern = rf"({section_title_regex})+([\W\w]*?)({section_title_regex})"
 
     return pattern
@@ -51,23 +56,28 @@ def rst_to_md(body: str):
     str
         A string containing the changelog title and content in markdown.
     """
-    # Remove the underlines
+    # Remove underline characters from the body
     underline_chars = ["=", "^"]
     for char in underline_chars:
         body = re.sub(rf"(\n|\r|\r\n)\{char}+", "", body)
 
-    # Style subsection titles
+    # Add "## " to the beginning of the release subsections.
+    # For example, "Added" becomes "## Added".
     body = re.sub(r"(?m)^(?=[a-zA-Z]+\s)", "## ", body)
 
-    # Link regex to match "`[#1234|0.1.2] <https://github.com/ansys/.../>`_"
+    # Find strings that match the format: "`[#1234|0.1.2] <https://github.com/ansys/.../>`_"
+    # This covers the release section and issue number with a link. For example:
+    # Release section with link: `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_
+    # Issue number with link: `#1234 <https://github.com/ansys/.../pull/1234>`_
     title_issue_regex = r"(`(\B#\d+|\d+.\d+.\d+)\W)(<(?<=\<)(.*?)(?=\>)>`_)"
     matches = re.findall(title_issue_regex, body)
 
+    # Change all RST links, `### <...>`_, to MD links, [###](...)
     for match in matches:
-        # Replace the first part of the link, either "`0.1.2 " or "#123", with "[0.1.2]" or "[#123]"
+        # Replace the first part of the link, either "`0.1.2 " or "#123 ", with "[0.1.2]" or "[#123]"
         body = body.replace(match[0], f"[{match[1]}]")
-        # Replace the link, "<https://github.com/ansys/abc/releases/tag/v0.11.8>`_", with
-        # "(https://github.com/ansys/abc/releases/tag/v0.11.8)"
+        # Replace the link, <https://github.com/ansys/.../releases/tag/v0.1.2>`_, with
+        # (https://github.com/ansys/.../releases/tag/v0.1.2)
         body = body.replace(match[2], f"({match[3]})")
 
     return body
@@ -96,17 +106,42 @@ def get_tag_section(changelog_file, body):
         # Get the regex pattern to grab the first section in the changelog
         # containing the section title and its subsections
         if file_type.lower() == "rst":
+            # For example, this pattern matches the following title:
+            # `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_ - 2024-10-30
             pattern = get_pattern(content, r"\`\d.* \<.*>\`.*")
         elif file_type.lower() == "md":
+            # For example, this pattern matches the following title:
+            # ## [0.1.2](https://github.com/ansys/.../releases/tag/v0.1.2) - 2024-10-30
             pattern = get_pattern(content, r"## \[\d.*\(.*\)*")
 
         if pattern:
             # Get all section titles and content
             matches = re.findall(pattern, content)
-            # Get the first section title (the most recent one)
+
+            # Get the first section of the changelog
             first_section = matches[0]
+            # The section title. For example:
+            # `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_ - 2024-10-30
+            title = first_section[0]
+            # The section content. For example:
+            # ========================================================================
+            #
+            # Added
+            # ^^^^^
+            #
+            # - New feature `#1234 <https://github.com/ansys/.../pull/1234>`_
+            section_content = first_section[1]
+
             # Combine the section title and its content into a string
-            body = first_section[0] + first_section[1]
+            # For example:
+            # `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_ - 2024-10-30
+            # ========================================================================
+            #
+            # Added
+            # ^^^^^
+            #
+            # - New feature `#1234 <https://github.com/ansys/.../pull/1234>`_
+            body = title + section_content
 
             # Convert rst to markdown
             if file_type.lower() == "rst":
