d85dd1a6843faf8b3b3da477aa97b8b57811b0aa
klmcadams <58492561+klmcadams@users.noreply.github.com>
2024-11-01 09:56:22 -0400

    fix regex & add semver into patterns

--- python-utils/release_github_utils.py
+++ python-utils/release_github_utils.py
@@ -1,6 +1,17 @@
 import re
 from pathlib import Path
 
+from parse_pr_title import get_towncrier_config_value, save_env_variable
+
+"""Semantic version regex as found on semver.org:
+https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string"""
+SEMVER_REGEX = (
+    r"(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)"
+    r"(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)"
+    r"(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?"
+    r"(?:\+(>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
+)
+
 
 def get_pattern(content, section_title_regex):
     """Get the regex for locating section titles and content.
@@ -38,9 +49,41 @@ def get_pattern(content, section_title_regex):
         # If there is more than one section
         elif len(sections) > 1:
             # Get the section title and its content up to the next section title
-            pattern = rf"({section_title_regex})+([\W\w]*?)({section_title_regex})"
+            pattern = rf"({section_title_regex})+([\W\w]*?)(?={section_title_regex})"
 
     return pattern
+
+
+def rst_to_md_links(body, original_number, new_number, original_link, new_link):
+    """Convert reStructuredText, RST, links to Markdown, MD, links.
+
+    Parameters
+    ----------
+    body: str
+        A string containing the section title and content of the tag with RST links.
+    original_number: str
+        The semantic version or issue number in RST format.
+        For example, "`0.1.2 " or "#123 "
+    new_number: str
+        The new semantic version or issue number in MD format.
+        For example, "## [0.1.2]" or "[#123]"
+    original_link: str
+        The link in RST format. For example, "<https://github.com/ansys/.../###>`_"
+    new_link: str
+        The link in MD format. For example, "(https://github.com/ansys/.../###)"
+
+    Returns
+    -------
+    str
+        A string containing the section title and content of the tag with MD links.
+    """
+    # Replace "`0.1.2 " with "## [0.1.2]" or "`#123 " with "[#123]"
+    body = body.replace(original_number, new_number)
+
+    # Replace "<https://github.com/ansys/.../###>`_" with "(https://github.com/ansys/.../###)"
+    body = body.replace(original_link, new_link)
+
+    return body
 
 
 def rst_to_md(body: str):
@@ -59,26 +102,35 @@ def rst_to_md(body: str):
     # Remove underline characters from the body
     underline_chars = ["=", "^"]
     for char in underline_chars:
-        body = re.sub(rf"(\n|\r|\r\n)\{char}+", "", body)
+        body = re.sub(rf"(\n|\r\n)\{char}+", "", body)
 
-    # Add "## " to the beginning of the release subsections.
-    # For example, "Added" becomes "## Added".
-    body = re.sub(r"(?m)^(?=[a-zA-Z]+\s)", "## ", body)
+    # Add "### " to the beginning of the release subsections.
+    # For example, "Added" becomes "### Added".
+    body = re.sub(r"(?m)^(?=[a-zA-Z]+\s)", "### ", body)
 
-    # Find strings that match the format: "`[#1234|0.1.2] <https://github.com/ansys/.../>`_"
-    # This covers the release section and issue number with a link. For example:
-    # Release section with link: `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_
-    # Issue number with link: `#1234 <https://github.com/ansys/.../pull/1234>`_
-    title_issue_regex = r"(`(\B#\d+|\d+.\d+.\d+)\W)(<(?<=\<)(.*?)(?=\>)>`_)"
-    matches = re.findall(title_issue_regex, body)
+    # Find the first string that matches the format:
+    # `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_
+    title_regex = re.search(rf"(`({SEMVER_REGEX})\W)(<(?<=\<)(.*?)(?=\>)>`_)", body)
+
+    # Fix the title link
+    body = rst_to_md_links(
+        body,
+        title_regex.group(1),
+        f"## [{title_regex.group(2)}]",
+        title_regex.group(8),
+        f"({title_regex.group(9)})",
+    )
+
+    # Find strings that match the format: "`#1234 <https://github.com/ansys/.../pull/1234>`_"
+    issue_regex = r"(`(\B#\d+)\W)(<(?<=\<)(.*?)(?=\>)>`_)"
+    matches = re.findall(issue_regex, body)
 
     # Change all RST links, `### <...>`_, to MD links, [###](...)
     for match in matches:
-        # Replace the first part of the link, either "`0.1.2 " or "#123 ", with "[0.1.2]" or "[#123]"
-        body = body.replace(match[0], f"[{match[1]}]")
-        # Replace the link, <https://github.com/ansys/.../releases/tag/v0.1.2>`_, with
-        # (https://github.com/ansys/.../releases/tag/v0.1.2)
-        body = body.replace(match[2], f"({match[3]})")
+        # Fix the issue links
+        body = rst_to_md_links(
+            body, match[0], f"[{match[1]}]", match[2], f"({match[3]})"
+        )
 
     return body
 
@@ -108,34 +160,18 @@ def get_tag_section(changelog_file, body):
         if file_type.lower() == "rst":
             # For example, this pattern matches the following title:
             # `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_ - 2024-10-30
-            pattern = get_pattern(content, r"\`\d.* \<.*>\`.*")
+            pattern = get_pattern(content, rf"\`{SEMVER_REGEX} \<.*>\`.*")
         elif file_type.lower() == "md":
             # For example, this pattern matches the following title:
-            # ## [0.1.2](https://github.com/ansys/.../releases/tag/v0.1.2) - 2024-10-30
-            pattern = get_pattern(content, r"## \[\d.*\(.*\)*")
+            # [0.1.2](https://github.com/ansys/.../releases/tag/v0.1.2) - 2024-10-30
+            pattern = get_pattern(content, rf"## \[{SEMVER_REGEX}]\(.*\)*")
 
         if pattern:
-            # Get all section titles and content
-            matches = re.findall(pattern, content)
+            # Find the first section title and content
+            match = re.search(pattern, content)
 
-            # Get the first section of the changelog
-            first_section = matches[0]
-
-            # The section title. For example:
-            # `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_ - 2024-10-30
-            title = first_section[0]
-
-            # The section content. For example:
-            # ========================================================================
-            #
-            # Added
-            # ^^^^^
-            #
-            # - New feature `#1234 <https://github.com/ansys/.../pull/1234>`_
-            section_content = first_section[1]
-
-            # Combine the section title and its content into a string
-            # For example:
+            # Access the match group containing the section title and content
+            # The match.group() could look like this, for example:
             # `0.1.2 <https://github.com/ansys/.../releases/tag/v0.1.2>`_ - 2024-10-30
             # ========================================================================
             #
@@ -143,7 +179,7 @@ def get_tag_section(changelog_file, body):
             # ^^^^^
             #
             # - New feature `#1234 <https://github.com/ansys/.../pull/1234>`_
-            body = title + section_content
+            body = match.group()
 
             # Convert rst to markdown
             if file_type.lower() == "rst":
@@ -152,3 +188,18 @@ def get_tag_section(changelog_file, body):
             print("Cannot generate release notes from changelog file.")
 
     return body
+
+
+def get_release_notes(pyproject_path):
+    # Set an empty string for the body of the release notes
+    body = ""
+    # Get the path to your changelog file: [tool.towncrier]'s filename configuration
+    changelog_loc = get_towncrier_config_value("filename", pyproject_path)
+    print(f"CHANGELOG_LOC: {changelog_loc}")
+
+    # If the changelog file exists, get the title and content for the release section
+    if changelog_loc:
+        body = get_tag_section(changelog_loc, body)
+
+    # Save the env variable
+    save_env_variable("RELEASE_NOTES_BODY", body)
