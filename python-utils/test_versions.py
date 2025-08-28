b83027140822ff203bc82d76375b2374226de696
Muhammed Adedigba <muhammed.adedigba@ansys.com>
2025-08-28 14:14:34 +0200

    feat: add more tests

--- python-utils/test_versions.py
+++ python-utils/test_versions.py
@@ -85,14 +85,23 @@ def test_environment_setup(request, tmp_path_factory, monkeypatch):
 
         return versions_json_path
 
-    def _create_prerelease_folder(prerelease_number: str):
+    def _create_release_folder(release_number: str):
         # Setup
-        prerelease_version_path = (
-            tmp_path_factory.getbasetemp() / "version" / prerelease_number
-        )
-        prerelease_version_path.mkdir()
+        release_version = Version(release_number)
+        if not release_version.is_prerelease:
+            release_number = f"{release_version.major}.{release_version.minor}"
 
-        return prerelease_version_path
+        release_version_path = (
+            tmp_path_factory.getbasetemp() / "version" / release_number
+        )
+
+        if not release_version.is_prerelease:
+            # The folder may already exist e.g. releasing 0.69.5 ==> 0.69 will be present already
+            release_version_path.mkdir(exist_ok=True)
+        else:
+            release_version_path.mkdir()
+
+        return release_version_path
 
     # Putting everything together
 
@@ -119,9 +128,7 @@ def test_environment_setup(request, tmp_path_factory, monkeypatch):
     create_versions_json_file = (
         True if test_data.get("create_versions_json_file") else False
     )
-    create_prerelease_folder = (
-        True if test_data.get("create_prerelease_folder") else False
-    )
+    create_release_folder = True if test_data.get("create_release_folder") else False
 
     if create_versions_directories:
         version_path = _create_versions_directories(versions_list)
@@ -132,17 +139,17 @@ def test_environment_setup(request, tmp_path_factory, monkeypatch):
     if create_versions_json_file:
         versions_json_path = _create_versions_json_file(versions_list)
 
-    if create_prerelease_folder:
+    if create_release_folder:
         version_number = (
             ref_name.split("v")[1] if "v" in ref_name else ref_name.split("/")[1]
         )
-        prerelease_version_path = _create_prerelease_folder(version_number)
+        release_version_path = _create_release_folder(version_number)
 
     yield request.param
 
     # Teardown
     if create_versions_directories:
-        # Note: this affects teardown of create_prerelease_folder
+        # Note: this affects teardown of create_release_folder
         # when both setups are used in a test because the entire version
         # directory is cleared.
         shutil.rmtree(version_path)
@@ -150,16 +157,16 @@ def test_environment_setup(request, tmp_path_factory, monkeypatch):
         gh_output_path.unlink()
     if create_versions_json_file:
         versions_json_path.unlink()
-    if create_prerelease_folder:
+    if create_release_folder:
         # Wrap this in a conditional because of the reason above
-        if prerelease_version_path.exists():
-            prerelease_version_path.rmdir()
+        if release_version_path.exists():
+            release_version_path.rmdir()
 
 
 ##########################################################################
 #         Generic tests, can be applied to multiple datasets             #
 ##########################################################################
-BASE_DATA = [
+BASE_PRERELEASE_DATA = [
     {
         "ref_type": "tag",
         "ref_name": "v0.4.0a0",
@@ -184,20 +191,69 @@ BASE_DATA = [
         "independent_patch_release": "false",
         "versions": ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6"],
     },
+    # Data involving jumps in release number (corner cases)
+    {
+        "ref_type": "tag",
+        "ref_name": "v1.2.0b0",
+        "independent_patch_release": "false",
+        "versions": ["0.1", "0.2", "0.3", "1.0", "1.1", "1.2.0a0"],
+    },
+    {
+        "ref_type": "branch",
+        "ref_name": "release/10.10.0b0",
+        "independent_patch_release": "false",
+        "versions": ["1.0", "1.1", "10.1", "10.9", "10.10.0a0"],
+    },
 ]
+
+BASE_NORMAL_RELEASE_DATA = [
+    {
+        "ref_type": "tag",
+        "ref_name": "v0.4.0",
+        "independent_patch_release": "false",
+        "versions": ["0.1", "0.2", "0.3"],
+    },
+    {
+        "ref_type": "tag",
+        "ref_name": "v0.55.5",
+        "independent_patch_release": "false",
+        "versions": ["0.50", "0.51", "0.52", "0.53", "0.54", "0.55"],
+    },
+    # Data involving jumps in release number (corner cases)
+    {
+        "ref_type": "tag",
+        "ref_name": "v1.1.0",
+        "independent_patch_release": "false",
+        "versions": ["0.1", "0.10", "0.11", "0.15", "1.0"],
+    },
+    {
+        "ref_type": "branch",
+        "ref_name": "release/2.0.0",
+        "independent_patch_release": "false",
+        "versions": ["0.1", "0.2", "0.5", "1.0", "1.5"],
+    },
+]
+
+BASE_DATA = deepcopy(BASE_PRERELEASE_DATA) + deepcopy(BASE_NORMAL_RELEASE_DATA)
 
 
 # helper function for some tests
-def expected_github_output(ref_name: str, without_patch_string: bool = False) -> str:
-    version_number = (
-        ref_name.split("v")[1] if "v" in ref_name else ref_name.split("/")[1]
-    )
-    version = Version(version_number)
-    prerelease = "true" if version.is_prerelease else "false"
-    if without_patch_string:
-        version_without_patch_string = f"{version.major}.{version.minor}"
-        return f"VERSION={version_without_patch_string}\nPRE_RELEASE={prerelease}\n"
-    return f"VERSION={version_number}\nPRE_RELEASE={prerelease}\n"
+def expected_github_output(ref_name: str, with_patch_string: bool = False) -> str:
+    ref_number = ref_name.split("v")[1] if "v" in ref_name else ref_name.split("/")[1]
+    ref_version = Version(ref_number)
+
+    if not ref_version.is_prerelease:
+        if with_patch_string:
+            version_string = f"{ref_version}"  # Normal independent patch release
+        else:
+            version_string = (
+                f"{ref_version.major}.{ref_version.minor}"  # Normal release
+            )
+    else:
+        version_string = f"{ref_version}"  # Pre-release
+    prerelease = "true" if ref_version.is_prerelease else "false"
+
+    return f"VERSION={version_string}\nPRE_RELEASE={prerelease}\n"
 
 
 @pytest.mark.parametrize("test_environment_setup", BASE_DATA, indirect=True)
@@ -235,10 +291,10 @@ def test_get_versions_list_default(test_environment_setup):
 
 # Modify base data, this test requires versions directory presence and existence of a prerelease
 # directory (can be from test data or by creating a directory for the current prerelease)
-BASE_DATA_TWO = deepcopy(BASE_DATA)
+BASE_DATA_TWO = deepcopy(BASE_PRERELEASE_DATA)
 for data in BASE_DATA_TWO:
     data["create_versions_directories"] = True
-    data["create_prerelease_folder"] = True
+    data["create_release_folder"] = True
 
 
 @pytest.mark.parametrize("test_environment_setup", BASE_DATA_TWO, indirect=True)
@@ -307,31 +363,6 @@ SPECIAL_TEST_DATA_ONE = [
     {
         "ref_type": "tag",
         "ref_name": "v0.4.0",
-        "independent_patch_release": "false",
-        "versions": ["0.1", "0.2", "0.3"],
-        "create_versions_directories": True,
-        "create_github_output_file": True,
-    }
-]
-
-
-@pytest.mark.parametrize("test_environment_setup", SPECIAL_TEST_DATA_ONE, indirect=True)
-def test_set_versions_variable_on_normal_release(test_environment_setup):
-    set_version_variable()
-    gh_output_path = os.getenv("GITHUB_OUTPUT")
-    gh_output_content = Path(gh_output_path).read_text()
-
-    test_data = test_environment_setup
-    ref_name = test_data["ref_name"]
-    expected_result = expected_github_output(ref_name, without_patch_string=True)
-
-    assert gh_output_content == expected_result
-
-
-SPECIAL_TEST_DATA_TWO = [
-    {
-        "ref_type": "tag",
-        "ref_name": "v0.4.0",
         "independent_patch_release": "true",
         "versions": ["0.1", "0.2", "0.3"],
         "create_versions_directories": True,
@@ -340,7 +371,7 @@ SPECIAL_TEST_DATA_TWO = [
 ]
 
 
-@pytest.mark.parametrize("test_environment_setup", SPECIAL_TEST_DATA_TWO, indirect=True)
+@pytest.mark.parametrize("test_environment_setup", SPECIAL_TEST_DATA_ONE, indirect=True)
 def test_set_versions_variable_on_independent_patch_release(test_environment_setup):
     set_version_variable()
     gh_output_path = os.getenv("GITHUB_OUTPUT")
@@ -348,12 +379,12 @@ def test_set_versions_variable_on_independent_patch_release(test_environment_set
 
     test_data = test_environment_setup
     ref_name = test_data["ref_name"]
-    expected_result = expected_github_output(ref_name)
+    expected_result = expected_github_output(ref_name, with_patch_string=True)
 
     assert gh_output_content == expected_result
 
 
-SPECIAL_TEST_DATA_THREE = [
+SPECIAL_TEST_DATA_TWO = [
     {
         "ref_type": "tag",
         "ref_name": "v0.4.0",
@@ -365,9 +396,7 @@ SPECIAL_TEST_DATA_THREE = [
 ]
 
 
-@pytest.mark.parametrize(
-    "test_environment_setup", SPECIAL_TEST_DATA_THREE, indirect=True
-)
+@pytest.mark.parametrize("test_environment_setup", SPECIAL_TEST_DATA_TWO, indirect=True)
 def test_prerelease_versions_clear_during_normal_release(test_environment_setup):
     set_version_variable()
     remaining_versions = get_versions_list()
@@ -383,7 +412,7 @@ def test_prerelease_versions_clear_during_normal_release(test_environment_setup)
     assert remaining_versions == expected_result
 
 
-SPECIAL_TEST_DATA_FOUR = [
+SPECIAL_TEST_DATA_THREE = [
     {
         "ref_type": "tag",
         "ref_name": "v0.4.0rc1",
@@ -396,7 +425,7 @@ SPECIAL_TEST_DATA_FOUR = [
 
 
 @pytest.mark.parametrize(
-    "test_environment_setup", SPECIAL_TEST_DATA_FOUR, indirect=True
+    "test_environment_setup", SPECIAL_TEST_DATA_THREE, indirect=True
 )
 def test_maximum_three_prerelease(test_environment_setup):
     set_version_variable()
