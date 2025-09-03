b308c939d5f3fdd134b5bdb1a172d1b04897e653
Muhammed Adedigba <muhammed.adedigba@ansys.com>
2025-09-03 11:00:45 +0200

    fix: codestyle

--- python-utils/zizmor-summary.py
+++ python-utils/zizmor-summary.py
@@ -13,11 +13,17 @@ def main():
     high = os.getenv("HIGH_AUDIT_LEVEL", "")
     strict = os.getenv("STRICT_AUDIT_LEVEL", "")
     if high not in ["--persona=pedantic", ""]:
-        raise ValueError(f'The value of HIGH_AUDIT_LEVEL environment variable should be "--persona=pedantic", not "{high}".')
+        raise ValueError(
+            f'The value of HIGH_AUDIT_LEVEL environment variable should be "--persona=pedantic", not "{high}".'
+        )
     if strict not in ["--persona=auditor", ""]:
-        raise ValueError(f'The value of STRICT_AUDIT_LEVEL environment variable should be "--persona=auditor", not "{strict}".')
-    if high and strict: # high and strict have both been set
-        raise ValueError("One of HIGH_AUDIT_LEVEL, STRICT_AUDIT_LEVEL environment variables should be set, not both.")
+        raise ValueError(
+            f'The value of STRICT_AUDIT_LEVEL environment variable should be "--persona=auditor", not "{strict}".'
+        )
+    if high and strict:  # high and strict have both been set
+        raise ValueError(
+            "One of HIGH_AUDIT_LEVEL, STRICT_AUDIT_LEVEL environment variables should be set, not both."
+        )
 
     cmd = ["zizmor", *shlex.split(high), *shlex.split(strict), "."]
 
